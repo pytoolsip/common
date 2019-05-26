01b7e2129e96596162ee2ce0370cc5fabbeda700
JimDreamHeart <15602291936@163.com>
2019-05-27 00:12:53 +0800

    refactor:修改校验更新平台版本方法

--- behavior/serviceBehavior/ServiceBehavior.py
+++ behavior/serviceBehavior/ServiceBehavior.py
@@ -15,7 +15,7 @@ def __getExposeData__():
 
 def __getExposeMethod__(DoType):
 	return {
-		"checkUpdateCommon" : DoType.AddToRear,
+		"checkUpdateIP" : DoType.AddToRear,
 		"autoLoginIP" : DoType.AddToRear,
 	};
 
@@ -37,15 +37,17 @@ class ServiceBehavior(_GG("BaseBehavior")):
 		self._className_ = ServiceBehavior.__name__;
 		pass;
 
-	# 检测更新Common
-	def checkUpdateCommon(self, obj, _retTuple = None):
-		resp = _GG("CommonClient").callService("Update", "UpdateReq", {"key" : _GG("AppConfig")["key"], "version" : _GG("AppConfig")["version"], "IPVersion" : _GG("AppConfig")["version"]});
+	# 检测更新平台
+	def checkUpdateIP(self, obj, _retTuple = None):
+		resp = _GG("CommonClient").callService("Update", "UpdateIPReq", {"uid" : _GG("CommonClient").getUserId(), "version" : _GG("AppConfig")["version"]});
 		if resp and not resp.isUpToDate:
-			msgDialog = wx.MessageDialog(obj, "检测有更新版本，是否确认更新？", "检测common版本！", style = wx.YES_NO|wx.ICON_QUESTION);
+			msgDialog = wx.MessageDialog(obj, "检测有更新版本，是否确认更新？", "检测平台版本！", style = wx.YES_NO|wx.ICON_QUESTION);
 			if msgDialog.ShowModal() == wx.ID_YES:
-				info = resp.updateInfo;
-				fileNme = info.url.split("/")[-1];
-				obj.download(info.url, _GG("g_ProjectPath")+"date/update/"+fileNme, info.totalSize);
+				dirPath = _GG("g_ProjectPath")+"date/update/pytoolsip/";
+				if not os.path.exists(dirPath):
+					os.mkdir(dirPath);
+				fileNme = resp.url.split("/")[-1];
+				obj.download(resp.url, dirPath+fileNme, resp.totalSize);
 
 	# 自动登录平台
 	def autoLoginIP(self, obj, _retTuple = None):
